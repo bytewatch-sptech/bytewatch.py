e8dbe94d32b8236647b92ea384859428d1d56c70
mtrbz <matheus.rbarros@sptech.school>
2026-03-10 23:04:27 -0300

    Atualização do script de escrita e adicionado script de leitura

--- escrita.py
+++ escrita.py
@@ -9,27 +9,45 @@ def criarArquivo():
     if not os.path.exists(arquivoCsv):
         with open('dados.csv', 'w', newline='') as arquivo:
             writer = csv.writer(arquivo)
-            writer.writerow(['data', 'cpu', 'ram', 'disco'])
+            writer.writerow(['data', '% Cpu', '% Cpu por nucleo', 'tempo cpu', 'processos ativos', 'ram total', 'ram usada', 'ram disponivel', '% Ram', '% Disco', 'disco disponivel', 'disco usado'])
 
         print(f'{arquivoCsv} criado com sucesso!')
 
 def capturarDados():
-    data = time.strftime('%B %d, %Y %H:%M:%S', time.localtime())
-    cpu = psutil.cpu_percent(interval=1)
-    mem = psutil.virtual_memory()
-    disk = psutil.disk_usage('/')
+    # Data
+    data = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    
+    # Métricas de CPU
+    pctCpu = psutil.cpu_percent(interval=1)
+    pctPerCpu = psutil.cpu_percent(percpu=True)
+    pctPerCpu = '|'.join(map(str, pctPerCpu))
+    tempoCpu = psutil.cpu_times()
+    processosAtivos = len(psutil.pids())
 
-    return data, cpu, mem, disk
+    # Métricas de RAM
+    mem = psutil.virtual_memory()
+    totalRam = mem.total
+    ramUsada = mem.used
+    ramDisponivel = mem.available
+    pctRam = mem.percent 
+
+    # Métricas de Disco
+    disk = psutil.disk_usage('/')
+    pctDisco = disk.percent
+    discoDisponivel = disk.free
+    discoUsado = disk.used
+
+    return data, pctCpu, pctPerCpu, tempoCpu, processosAtivos, totalRam, ramUsada, ramDisponivel, pctRam, pctDisco, discoDisponivel, discoUsado
 
 def atualizarArquivo():
     criarArquivo()
 
     while True:
-        data, cpu, mem, disk = capturarDados()
+        data, pctCpu, pctPerCpu, tempoCpu, processosAtivos, totalRam, ramUsada, ramDisponivel, pctRam, pctDisco, discoDisponivel, discoUsado = capturarDados()
 
         with open(arquivoCsv, 'a', newline='') as arquivo:
             writer = csv.writer(arquivo)
-            writer.writerow([data, cpu, mem.percent, disk.percent])
+            writer.writerow([data, pctCpu, pctPerCpu, tempoCpu, processosAtivos, totalRam, ramUsada, ramDisponivel, pctRam, pctDisco, discoDisponivel, discoUsado])
 
             print('Atualizando arquivo...')
             time.sleep(1)
