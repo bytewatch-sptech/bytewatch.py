687c1f2312a98a8b23c9d962f040f6f60f1de000
luisfelip2005 <gomesluisfelipe09@gmail.com>
2026-04-15 19:47:17 -0300

    arrumando colunas de media

--- leitura.py
+++ leitura.py
@@ -82,11 +82,11 @@ class Leitura:
         self.dataframe["discoUsado"] = round(self.dataframe["discoUsado"] / 1024**3, 2)
         self.dataframe["discoTotal"] = round(self.dataframe["discoTotal"] / 1024**3, 2)
 
-        self.dataframe["mediaRam"] = round(self.dataframe["ramUsada"].mean() / 1024**3, 2)
+        self.dataframe["mediaRam"] = round(self.dataframe["ramUsada"].mean(), 2)
+        self.dataframe["mediaDisco"] = round(self.dataframe["discoUsado"].mean(), 2)
         self.dataframe["percentualRam"] = round((self.dataframe["ramUsada"] / self.dataframe["ramTotal"]) * 100, 2)
-        self.dataframe["mediaDisco"] = round(self.dataframe["discoUsado"].mean() / 1024**3, 2)
         self.dataframe["percentualDisco"] = round((self.dataframe["discoUsado"] / self.dataframe["discoTotal"]) * 100, 2)
-
+        
         self.dataframe["bytesEnviados"] = round(self.dataframe["bytesEnviados"] / 1024**2, 2)
         self.dataframe["bytesRecebidos"] = round(self.dataframe["bytesRecebidos"] / 1024**2, 2)
         self.dataframe["velocidadeDownload"] = round(self.dataframe["velocidadeDownload"])
