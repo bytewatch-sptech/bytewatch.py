f48d593b1ea2eb42a01f751e61107eef6ad8d1c6
luisfelip2005 <gomesluisfelipe09@gmail.com>
2026-04-25 19:25:17 -0300

    capturando dropped packets e active conn

--- escrita.py
+++ escrita.py
@@ -55,7 +55,11 @@ class Escrita():
         velocidadeDownload = (bytesRecebidos_2 - bytesRecebidos)
         velocidadeUpload = (bytesEnviados_2 - bytesEnviados)
 
-        return bytesRecebidos, bytesEnviados, velocidadeDownload, velocidadeUpload
+        droppedPackets = net_io.dropin + net_io.dropout
+
+        conexoes_ativas = len(psutil.net_connections())
+
+        return bytesRecebidos, bytesEnviados, velocidadeDownload, velocidadeUpload, droppedPackets, conexoes_ativas
     
     def capturarProcessos(self):
         num_cpus = psutil.cpu_count()
@@ -99,8 +103,8 @@ class Escrita():
         discoUsado = (psutil.disk_usage("/").used)
         discoTotal = (psutil.disk_usage("/").total)
 
-        bytesRecebidos, bytesEnviados, velocidadeDownload, velocidadeUpload = self.pegarVelocidadeInternet()
+        bytesRecebidos, bytesEnviados, velocidadeDownload, velocidadeUpload, droppedPackets, conexoes_ativas = self.pegarVelocidadeInternet()
 
         print(f"|    Escrita de métricas\n|    Horário: {horas}\n|    Endereço MAC: {self.macAddress}\n|    Nome da máquina: {nome_maquina}\n|\n======================================\n\n                CPU\n\nProcessador: {processador}\n\nCPU Porcentagem: {cpuPorcentagem}%\nCPU Núcleos Físicos: {cpuNucleosFisicos}\nCPU Núcleos Lógicos: {cpuNucleosLogicos}\n\nCPU Tempo Usuário: {cpuTempoUser}\nCPU Tempo Sistema: {cpuTempoSistema}\nCPU Tempo Inativo: {cpuTempoInativo}\n\n----------------------------------\n                RAM\n\nRAM Usada: {ramUsada}\nRAM Total: {ramTotal}\nRAM Livre: {ramLivre}\n\n----------------------------------\n                DISCO\n\nDisco Usado: {discoUsado}\nDisco Total: {discoTotal}\nDisco Livre: {discoLivre}\n\n----------------------------------\n                INTERNET\n\nTotal Bytes Enviados: {bytesEnviados}\nTotal Bytes Recebidos: {bytesRecebidos}\nVelocidade Download: {velocidadeDownload} B/s\nvelocidade Upload: {velocidadeUpload} B/s\n\n======================================")
 
-        return {"horario": [horas], "macAddress": self.macAddress, "nome_maquina": [nome_maquina], "processador": [processador], "cpuPorcentagem": [cpuPorcentagem], "cpuNucleosFisicos": [cpuNucleosFisicos], "cpuNucleosLogicos": [cpuNucleosLogicos], "cpuTempoUser": [cpuTempoUser], "cpuTempoSistema": [cpuTempoSistema], "cpuTempoInativo": [cpuTempoInativo], "ramUsada": [ramUsada], "ramTotal": [ramTotal], "ramLivre": [ramLivre], "discoUsado": [discoUsado], "discoTotal": [discoTotal], "discoLivre": [discoLivre], "bytesEnviados": [bytesEnviados], "bytesRecebidos": [bytesRecebidos], "velocidadeDownload": [velocidadeDownload], "velocidadeUpload": [velocidadeUpload]}
+        return {"horario": [horas], "macAddress": self.macAddress, "nome_maquina": [nome_maquina], "processador": [processador], "cpuPorcentagem": [cpuPorcentagem], "cpuNucleosFisicos": [cpuNucleosFisicos], "cpuNucleosLogicos": [cpuNucleosLogicos], "cpuTempoUser": [cpuTempoUser], "cpuTempoSistema": [cpuTempoSistema], "cpuTempoInativo": [cpuTempoInativo], "ramUsada": [ramUsada], "ramTotal": [ramTotal], "ramLivre": [ramLivre], "discoUsado": [discoUsado], "discoTotal": [discoTotal], "discoLivre": [discoLivre], "bytesEnviados": [bytesEnviados], "bytesRecebidos": [bytesRecebidos], "velocidadeDownload": [velocidadeDownload], "velocidadeUpload": [velocidadeUpload], "droppedPackets": [droppedPackets], "conexoesAtivas": [conexoes_ativas]}
